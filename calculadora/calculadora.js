366838a37663dddf254720cc2cc5c3020817cf62
PedroPalandi <pedrohenriquepalandi@gmail.com>
2026-03-04 16:51:00 -0300

    feat: Opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,29 +1,43 @@
-function calculadora(){
-    while(true){
-        let resultado = 0.0;
-        let operando1;
-        let operando2;
-        let operador;
+    function calculadora(){
+        while(true){
+            let resultado = 0.0;
+            let operando1;
+            let operando2;
+            let operador;
+            let continua;
+            let operador_valido = true;
 
-        operando1 = parseFloat( prompt("Digite o primeiro número: ") );
-        operando2 = parseFloat(prompt("Digite o segundo número: ") );
-        operador = prompt("Digite uma das operações ( + - * / ): ");
+            operando1 = parseFloat( prompt("Digite o primeiro número: ") );
+            operando2 = parseFloat(prompt("Digite o segundo número: ") );
+            operador = prompt("Digite uma das operações ( + - * / ): ");
 
-        if ( operador === "+"  ){
-            resultado = operando1 + operando2;
-        } else if (operador === "-") {
-            resultado = operando1 - operando2;
-        } else if (operador ==="*"){
-            resultado = operando1 * operando2;
-        } else if (operador === "/"){
-            resultado = operando1 / operando2;
-        } else {
-            alert("Operado inválido");
-            return
-        }
-
-        alert("Resultado: " + 
-            operando1 + " " + operador + " " + operando2 + " = " + resultado);
-    }        
-}
-
+            if ( operador === "+"  ){
+                resultado = operando1 + operando2;
+            } else if (operador === "-") {
+                resultado = operando1 - operando2;
+            } else if (operador ==="*"){
+                resultado = operando1 * operando2;
+            } else if (operador === "/"){
+                if (operando2 == 0){
+                    alert("Não é possivel dividir por zero.")
+                    continua = prompt("Digite sim para continuar e não para encerrar")
+                    if(continua === "não"){
+                        return
+                    } 
+                } else {
+                    resultado = operando1 / operando2;
+                }
+            
+            } else {
+                alert("Operador inválido");
+                operador_valido = false;
+                 continua = prompt("Digite sim para continuar e não para encerrar")
+                return
+            }
+            if( operador != "/" || operando2 != 0 ){
+                alert("Resultado: " + 
+                operando1 + " " + operador + " " + operando2 + " = " + resultado);
+            } 
+            
+        }        
+    }
