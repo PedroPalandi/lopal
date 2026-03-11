1bafa6ea3a2c97b8b4947567da193bbb73271799
PedroPalandi <pedrohenriquepalandi@gmail.com>
2026-03-11 16:41:23 -0300

    feat: Calculadora terminada e Iniciada Urna Eletrônica

---
+++ Idade/idade.js
@@ -0,0 +1,8 @@
+function idade(){
+    let idade;
+    do{
+        idade = parseInt(prompt("Informe sua idade (valores aceitos de 5 a 150):"));
+    } while( idade < 5 || idade > 150 || idade != idade  );
+    alert("Idade validada");
+
+}
